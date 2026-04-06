281ce0eee0a3256f98829a306fd228ee33a24b2e
Luke Marchant <gswebmaster@byu.edu>
2026-04-06 17:08:50 -0600

    Got backend deployed

--- backend/Lighthouse.Web/Controllers/HomeController.cs
+++ backend/Lighthouse.Web/Controllers/HomeController.cs
@@ -8,51 +8,12 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        ViewBag.Page = "home";
-        return View("ReactApp");
-    }
-
-    [HttpGet]
-    public IActionResult Impact()
-    {
-        ViewBag.Page = "impact";
-        return View("ReactApp");
-    }
-
-    [HttpGet]
-    public IActionResult About()
-    {
-        ViewBag.Page = "about";
-        return View("ReactApp");
-    }
-
-    [HttpGet]
-    public IActionResult Contact()
-    {
-        ViewBag.Page = "contact";
-        return View("ReactApp");
-    }
-
-    [HttpGet]
-    public IActionResult Privacy()
-    {
-        ViewBag.Page = "privacy";
-        return View("ReactApp");
-    }
-
-    [HttpPost]
-    [IgnoreAntiforgeryToken]
-    public IActionResult Contact(string name, string email, string message)
-    {
-        var ok = !string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(message);
-        ViewBag.Page = "contact";
-        ViewBag.ContactSent = ok;
-        return View("ReactApp");
+        return Ok("Lighthouse API is running!");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return Problem(detail: "An error occurred in the Lighthouse API.");
     }
 }
